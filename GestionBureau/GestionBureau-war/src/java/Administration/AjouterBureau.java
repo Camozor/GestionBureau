f8edee13169e1bfd854ab738056a487554cd6d8b
antoinekagit <travail@antoineka.fr>
2014-12-18 18:00:26 +0100

    renommé AdminGestionTruc en ajouterTruc

--- GestionBureau/GestionBureau-war/src/java/Administration/AjouterBureau.java
+++ GestionBureau/GestionBureau-war/src/java/Administration/AjouterBureau.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author 11316834
  */
-public class AdminGestionBureau extends HttpServlet {
+public class AjouterBureau extends HttpServlet {
     @EJB
     private BureauFacadeLocal bureauFacade;
 
